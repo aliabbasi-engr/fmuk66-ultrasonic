430b6b22ba8a52100d6c4f219d4667c8cdd1aa8d
Ali Abbasi <78269670+aliabbasi-engr@users.noreply.github.com>
2023-05-03 15:46:02 -0400

    Update hello_world.c

--- workspace/frdmk66f_hello_world/source/hello_world.c
+++ workspace/frdmk66f_hello_world/source/hello_world.c
@@ -3,6 +3,8 @@
 #include "board.h"
 #include "fsl_debug_console.h"
 #include "fsl_gpio.h"
+#include "math.h"
+#include "stdio.h"
 
 /*******************************************************************************
  * Definitions
@@ -15,12 +17,23 @@
 
 #define ULTRASONIC_ECHO_VALUE		1U
 
+#define ULTRASONIC_ECHO_PROPAGATION_DELAY	35
+
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
 /*!
  * @brief delay a while.
  */
+void gpio_configure(void);
+void delay(void);
+void reset_timer(void);
+void start_timer(void);
+void stop_timer(void);
+unsigned int get_cycles(void);
+int read_echo(void);
+int get_int_part(float);
+int get_float_part(float, int);
 
 /*******************************************************************************
  * Variables
@@ -32,6 +45,61 @@ volatile unsigned int *SCB_DEMCR   ;
 /*******************************************************************************
  * Code
  ******************************************************************************/
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    /* Board pin, clock, debug console init */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitDebugConsole();
+
+    gpio_configure();
+
+    int elapsed_cycles;
+    float elapsed_time_us;
+    float distance_cm;
+
+    /* Print a note to terminal. */
+    PRINTF("\r\n** Ultrasonic Driver Example **\r\n");
+
+    delay();
+
+    while (1)
+    {
+    	reset_timer();
+    	GPIO_PortSet(ULTRASONIC_TRIGGER_GPIO, 1u << ULTRASONIC_TRIGGER_GPIO_PIN);
+    	start_timer();
+
+        while (get_cycles() <= 180) {}
+
+        GPIO_PortClear(ULTRASONIC_TRIGGER_GPIO, 1u << ULTRASONIC_TRIGGER_GPIO_PIN);
+        stop_timer();
+
+        reset_timer();
+        while (read_echo() != ULTRASONIC_ECHO_VALUE) {}
+        start_timer();
+
+        while (read_echo() == ULTRASONIC_ECHO_VALUE) {}
+
+        stop_timer();
+        elapsed_cycles = get_cycles();
+        //PRINTF("Elapsed cycles = %d\r\n", elapsed_cycles);
+
+        elapsed_time_us = 0.0555555555555556f * elapsed_cycles;
+
+        elapsed_time_us = elapsed_time_us - ULTRASONIC_ECHO_PROPAGATION_DELAY;
+
+        //PRINTF("Elapsed time = %d.%dus\r\n", get_int_part(elapsed_time_us), get_float_part(elapsed_time_us, 2));
+
+        distance_cm = elapsed_time_us * 0.034f / 2;
+        PRINTF("Distance = %d.%dcm\r\n", get_int_part(distance_cm), get_float_part(distance_cm, 2));
+
+        delay();
+    }
+}
+
 void gpio_configure(void)
 {
     /* Define the init structure for the ultrasonic trigger output pin*/
@@ -56,89 +124,50 @@ void gpio_configure(void)
 void delay(void)
 {
     volatile uint32_t i = 0;
-    for (i = 0; i < 800000; ++i)
+    for (i = 0; i < 80000; ++i)
     {
         __asm("NOP"); /* delay */
     }
 }
 
-void reset_timer()
+void reset_timer(void)
 {
-    DWT_CYCCNT   = (int *)0xE0001004; //address of the register
-    DWT_CONTROL  = (int *)0xE0001000; //address of the register
-    SCB_DEMCR    = (int *)0xE000EDFC; //address of the register
+    DWT_CYCCNT   = (int *)0xE0001004;
+    DWT_CONTROL  = (int *)0xE0001000;
+    SCB_DEMCR    = (int *)0xE000EDFC;
     *SCB_DEMCR   = *SCB_DEMCR | 0x01000000;
     *DWT_CYCCNT  = 0; // reset the counter
     *DWT_CONTROL = 0;
 }
 
-void start_timer()
+void start_timer(void)
 {
-    *DWT_CONTROL = *DWT_CONTROL | 1 ; // enable the counter
+    *DWT_CONTROL = *DWT_CONTROL | 1 ;
 }
 
-void stop_timer()
+void stop_timer(void)
 {
-    *DWT_CONTROL = *DWT_CONTROL | 0 ; // disable the counter
+    *DWT_CONTROL = *DWT_CONTROL | 0 ;
 }
 
-unsigned int get_cycles()
+unsigned int get_cycles(void)
 {
     return *DWT_CYCCNT;
 }
 
-int read_echo()
+int read_echo(void)
 {
 	return GPIO_PinRead(ULTRASONIC_ECHO_GPIO, ULTRASONIC_ECHO_GPIO_PIN);
 }
 
-/*!
- * @brief Main function
- */
-int main(void)
+int get_int_part(float val)
 {
-    /* Board pin, clock, debug console init */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitDebugConsole();
+    return (int)val;
+}
 
-    gpio_configure();
-
-    int elapsed_cycles;
-    float elapsed_time_us;
-    float distance_cm;
-
-    /* Print a note to terminal. */
-    PRINTF("\r\n Ultrasonic Driver example\r\n");
-
-    delay();
-
-    while (1)
-    {
-    	PRINTF("\r\n Sending the trigger signal.\r\n");
-
-    	reset_timer(); //reset timer
-    	GPIO_PortSet(ULTRASONIC_TRIGGER_GPIO, 1u << ULTRASONIC_TRIGGER_GPIO_PIN);
-    	start_timer(); //start timer
-
-        while (get_cycles() <= 180) {}
-
-        GPIO_PortClear(ULTRASONIC_TRIGGER_GPIO, 1u << ULTRASONIC_TRIGGER_GPIO_PIN);
-        stop_timer(); //stop timer
-
-        /*reset_timer(); //reset timer
-        while (read_echo() != ULTRASONIC_ECHO_VALUE) {}
-        start_timer(); //start timer
-
-        while (read_echo() == ULTRASONIC_ECHO_VALUE) {}
-
-        stop_timer();
-        elapsed_cycles = get_cycles();
-
-        elapsed_time_us = 0.0555f * elapsed_cycles;
-
-        distance_cm = elapsed_time_us * 0.034f / 2;
-
-        PRINTF("Distance = %dcm", distance_cm);*/
-    }
+int get_float_part(float val, int precision)
+{
+	int int_part = (int)val;
+	float float_part = val - (float)int_part;
+	return float_part * pow(10, precision);
 }
